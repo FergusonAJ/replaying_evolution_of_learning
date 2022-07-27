e9582f0cdf062ce584b1524293407de10e13ac3f
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-07-27 17:57:21 -0400

    Add experiment framework & first 2 experiments

---
+++ config_global.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This file contains variables that are shared across ALL experiments
+
+# Grab the full path to the root directory of the repository 
+REPO_ROOT_DIR=
+# All files on the scratch will live in some subdirectory of this
+SCRATCH_ROOT_DIR=
