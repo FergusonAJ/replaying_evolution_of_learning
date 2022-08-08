a1b2a939fd4e97990753bdc626104ff816765b7b
Austin Ferguson <ferguson.austin.j@gmail.com>
2022-08-08 14:08:30 -0400

    Update various bash scripts

--- experiments/exploration/2022_07_27__two_cues_phylo/data/scripts/scrape_final_dominant_data.sh
+++ experiments/exploration/2022_07_27__two_cues_phylo/data/scripts/scrape_final_dominant_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IS_REPLAY=1
-REPLAY_SEED=7
+#REPLAY_SEED=7
 #REPLAY_DEPTH=16  # Scraped 
 #REPLAY_DEPTH=33  # Scraped
 #REPLAY_DEPTH=49  # Scraped
@@ -12,12 +12,24 @@ REPLAY_SEED=7
 #REPLAY_DEPTH=133 # Scraped
 #REPLAY_DEPTH=149 # Scraped
 #REPLAY_DEPTH=166 # Scraped
-REPLAY_DEPTH=182 # Scraped
+#REPLAY_DEPTH=182 # Scraped
 #REPLAY_DEPTH=199 # Scraped (100% learning)
 #REPLAY_DEPTH=266 # Scraped
 #REPLAY_DEPTH=333 # Scraped
 #REPLAY_DEPTH=399 # Scraped
 
+#REPLAY_SEED=69
+#REPLAY_DEPTH=80   # 4  Scraped 
+#REPLAY_DEPTH=160  # 2  Scraped 
+#REPLAY_DEPTH=200  # 7  Scraped 
+#REPLAY_DEPTH=240  # 5  Scraped 
+#REPLAY_DEPTH=280  # 8  Scraped 
+#REPLAY_DEPTH=320  # 1  Scraped
+#REPLAY_DEPTH=360  # 9  Scraped 
+#REPLAY_DEPTH=400  # 6  Scraped 
+#REPLAY_DEPTH=440  # 10 Scraped 
+#REPLAY_DEPTH=480  # 3  Scraped 
+    
 # Grab the experiment name
 EXP_NAME=$(pwd | grep -oP "/\K[^/]+(?=/data|/data/scripts)")
 
@@ -25,20 +37,25 @@ EXP_NAME=$(pwd | grep -oP "/\K[^/]+(?=/data|/data/scripts)")
 REPO_ROOT_DIR=$(pwd | grep -oP ".+/(?=experiments/)")
 source ${REPO_ROOT_DIR}/config_global.sh
 
-# Calculate directories to pass to R script
-SCRATCH_REP_DIR=${SCRATCH_ROOT_DIR}/${EXP_NAME}/reps
-OUTPUT_DIR=$(pwd | grep -oP ".+/${EXP_NAME}/data")
-SCRIPT_DIR=${OUTPUT_DIR}/scripts
+REPLAY_SEED=82
+for REPLAY_DEPTH in 40 80 120 160 200 220 240 260 280 300
+do
 
-if [ $IS_REPLAY -gt 0 ]
-then
-    SCRATCH_REP_DIR=${SCRATCH_REP_DIR}/${REPLAY_SEED}/replays/${REPLAY_DEPTH}
-    OUTPUT_DIR=${OUTPUT_DIR}/reps/${REPLAY_SEED}/replays/${REPLAY_DEPTH}
-    mkdir -p ${OUTPUT_DIR}
-fi
+    # Calculate directories to pass to R script
+    SCRATCH_REP_DIR=${SCRATCH_ROOT_DIR}/${EXP_NAME}/reps
+    OUTPUT_DIR=$(pwd | grep -oP ".+/${EXP_NAME}/data")
+    SCRIPT_DIR=${OUTPUT_DIR}/scripts
 
-echo ""
-echo "Pulling data from: ${SCRATCH_REP_DIR}"
-echo "Saving data to: ${OUTPUT_DIR}"
-echo ""
-Rscript ${SCRIPT_DIR}/combine_final_dominant_data.R ${SCRATCH_REP_DIR} ${OUTPUT_DIR}
+    if [ $IS_REPLAY -gt 0 ]
+    then
+        SCRATCH_REP_DIR=${SCRATCH_REP_DIR}/${REPLAY_SEED}/replays/${REPLAY_DEPTH}
+        OUTPUT_DIR=${OUTPUT_DIR}/reps/${REPLAY_SEED}/replays/${REPLAY_DEPTH}
+        mkdir -p ${OUTPUT_DIR}
+    fi
+
+    echo ""
+    echo "Pulling data from: ${SCRATCH_REP_DIR}"
+    echo "Saving data to: ${OUTPUT_DIR}"
+    echo ""
+    Rscript ${SCRIPT_DIR}/combine_final_dominant_data.R ${SCRATCH_REP_DIR} ${OUTPUT_DIR}
+done
