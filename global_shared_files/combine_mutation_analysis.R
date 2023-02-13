040741397523fa93f7ad1c9d57ab3a7c02e3b47d
Austin Ferguson <ferguson.austin.j@gmail.com>
2023-02-13 13:38:44 -0500

    Add script to combine mutation class data

---
+++ global_shared_files/combine_mutation_analysis.R
@@ -0,0 +1,45 @@
+rm(list = ls())
+
+# If we're in RStudio (i.e., local), hardcode directories
+if(Sys.getenv("RSTUDIO") == 1){
+  input_dir = '../data/reps/28/mutants/391'
+  output_dir = input_dir
+} else { # Else pull from command line args
+  args = commandArgs(trailingOnly=TRUE)
+  if(length(args) < 2){
+    cat('Error! combine_mutation_classification.R expects exactly 2 command line arguments:\n')
+    cat('    1. The input directory\n')
+    cat('    2. The output directory\n')
+  }
+  script_dir = args[1]
+  input_dir = args[2]
+  output_dir = args[3]
+  seed = as.numeric(args[4])
+  depth = as.numeric(args[5])
+  mut_job_id = as.numeric(args[6])
+}
+
+df = NA
+for(job_id in 1:3){
+  input_filename = paste0(input_dir, '/mut_classification_', job_id, '.csv')
+  df_tmp = read.csv(input_filename)
+  if(!is.data.frame(df)){
+    df = df_tmp
+  } else {
+    for(row_idx in 1:nrow(df_tmp)){
+      row = df_tmp[row_idx,]
+      if(row$seed_classification %in% df$seed_classification){
+        mask = df$seed_classification == row$seed_classification
+        new_count = row$count + df[mask,]$count
+        new_merit = ((row$merit_mean_mean * row$count) + (df[mask,]$merit_mean_mean * df[mask,]$count)) / new_count
+        new_accuracy = ((row$accuracy_mean_mean * row$count) + (df[mask,]$accuracy_mean_mean * df[mask,]$count)) / new_count
+        df[mask,]$count = new_count
+        df[mask,]$merit_mean_mean = new_merit
+        df[mask,]$accuracy_mean_mean = new_accuracy
+      } else {
+        df[nrow(df) + 1,] = df
+      }
+    }
+  }
+}
+
