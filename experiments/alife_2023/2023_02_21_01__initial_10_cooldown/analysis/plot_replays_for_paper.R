1ec5e66df6edf23e5ecc301af1613a41185d12b1
Austin Ferguson <ferguson.austin.j@gmail.com>
2023-06-27 15:03:56 -0400

    Make font size a variable

--- experiments/alife_2023/2023_02_21_01__initial_10_cooldown/analysis/plot_replays_for_paper.R
+++ experiments/alife_2023/2023_02_21_01__initial_10_cooldown/analysis/plot_replays_for_paper.R
@@ -51,13 +51,14 @@ mask_initial = df$is_initial
 mask_targeted = df$is_targeted
 
 margin_top = 0.2
-point_size = 1.5
+point_size = 1.3
 font_size_small = 12
 font_size_large = 14
+line_size = 0.7
 
 ggplot(df, aes(x=depth)) + 
       #geom_vline(aes(xintercept = focal_target), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = 2.5) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -77,7 +78,7 @@ if(T){
   ggp_4_init = ggplot(df[mask_initial & df$seed == 4,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['4'], xmax=focal_stop_map['4'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       #geom_vline(aes(xintercept = focal_target), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -97,7 +98,7 @@ if(T){
   ggp_4_target = ggplot(df[mask_targeted & df$seed == 4,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['4'], xmax=focal_stop_map['4'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       geom_vline(aes(xintercept = focal_target_map['4']), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -119,7 +120,7 @@ if(T){
   ggp_6_init = ggplot(df[mask_initial & df$seed == 6,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['6'], xmax=focal_stop_map['6'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       #geom_vline(aes(xintercept = focal_target), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -139,7 +140,7 @@ if(T){
   ggp_6_target = ggplot(df[mask_targeted & df$seed == 6,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['6'], xmax=focal_stop_map['6'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       geom_vline(aes(xintercept = focal_target_map['6']), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -161,7 +162,7 @@ if(T){
   ggp_15_init = ggplot(df[mask_initial & df$seed == 15,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['15'], xmax=focal_stop_map['15'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       #geom_vline(aes(xintercept = focal_target), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -181,7 +182,7 @@ if(T){
   ggp_15_target = ggplot(df[mask_targeted & df$seed == 15,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['15'], xmax=focal_stop_map['15'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       geom_vline(aes(xintercept = focal_target_map['15']), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
@@ -203,7 +204,7 @@ if(T){
   ggp_86_init = ggplot(df[mask_initial & df$seed == 86,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['86'], xmax=focal_stop_map['86'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       #geom_vline(aes(xintercept = focal_target), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map, breaks = c(seed_class_learning, seed_class_bet_hedged_learning, seed_class_error_correction, seed_class_bet_hedged_error_correction, seed_class_bet_hedged_mixed, seed_class_small)) + 
@@ -223,7 +224,7 @@ if(T){
   ggp_86_target = ggplot(df[mask_targeted & df$seed == 86,], aes(x=depth)) + 
       annotate("rect", xmin=focal_start_map['86'], xmax=focal_stop_map['86'], ymin=-Inf, ymax=Inf, alpha=0.2, fill="#64e164") +
       geom_vline(aes(xintercept = focal_target_map['86']), linetype = 'dotted') +
-      geom_line(mapping=aes(y = frac*100), size = 1.05) + 
+      geom_line(mapping=aes(y = frac*100), size = line_size) + 
       geom_point(mapping=aes(y = frac*100, color = as.factor(lineage_classification)), size = point_size) + 
       #geom_rect(data=df_lineage[mask_initial_lineage,], mapping = aes(xmin = depth,xmax = depth+1, ymin = -0.075,ymax = -0.05, fill = seed_classification_factor), size = 0.1) +
       scale_color_manual(values = color_map) + 
